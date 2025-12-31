24bb776dbd1c542191c6599b8af123bec47ca6db
Shubham Rajesh Saroj <57361979+shubhamrsaroj@users.noreply.github.com>
2025-12-31 11:03:26 +0530

    some exp changes

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -3,6 +3,6 @@ export const experiences = [
     id: 1,
     title: "Full Stack Developer",
     company: "InstaCharter Private Limited",
-    duration: "(Aug 2024 - Sep 2025)"
+    duration: "(Sep 2024 - Sep 2025)"
   }
 ]
